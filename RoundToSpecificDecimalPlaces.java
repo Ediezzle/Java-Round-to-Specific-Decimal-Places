04aad9e7f4bbf57823c6ba761909a194f6aabac1
Ediezzle <edmore.mandikiyana@gmail.com>
2020-01-11 13:45:02 +0200

    final commit

---
+++ RoundToSpecificDecimalPlaces.java
@@ -0,0 +1,13 @@
+public class RoundToSpecificDecimalPlaces
+{
+	public static void main(String[] args)
+	{
+		float value = 12.349120f;
+		double val = 114.798614;
+		System.out.printf("%.2f\n", value);
+		System.out.printf("%.2f\n", val);
+	}
+}
+
+
+
